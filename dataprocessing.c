225823e47fc978856a2da16bf8690c4df2ab4d32
Xing Yin <yinxing1995@163.com>
2021-07-03 16:14:50 +0100

    data interface added

--- dataprocessing.c
+++ dataprocessing.c
@@ -10,9 +10,58 @@
 static int State = Init;
 static char frameflag[sizeof(FRAMEFLAG)];
 static uint16_t FrameLen = 0;
-char *fbuf = NULL;
+static ClusterArray *Head = NULL;
+static char *fbuf = NULL;
 
-
+static void DataPush(char *buf, int len)
+{
+    ClusterArray * Current = NULL;
+    uint8_t node = *buf++;
+    uint8_t cluster = *buf++;
+    uint8_t endpoint = *buf++;
+    if(!Head)
+    {
+        Head = (ClusterArray *)malloc(sizeof(ClusterArray));
+        Current = Head;
+        Current->next = NULL;
+    }
+    else
+    {
+        Current = Head;
+        while(!((Current->NodeID == node)&&(Current->ClusterID == cluster)&&(Current->Endpoint == endpoint)))
+        {
+            if(Current->next == NULL)
+            {
+                Current->next = (ClusterArray *)malloc(sizeof(ClusterArray));
+                Current = Current->next;
+                Current->next = NULL;
+                break;
+            }
+            Current = Current->next;
+        }
+    }
+    Current->NodeID = node;
+    Current->ClusterID = cluster;
+    Current->Endpoint = endpoint;
+    Current->ControlFlag = *buf++;
+    Current->DataType = *buf++;
+    Current->DataLength = *buf++;
+    switch(Current->DataType)
+    {
+        case _INT:
+            Current->Data = malloc(Current->DataLength*sizeof(int));
+            break;
+        case _FLOAT:
+            Current->Data = malloc(Current->DataLength*sizeof(float));
+            break;
+        default:
+            printf("Unknown type\r\n");
+            break;
+    }
+    memcpy(Current->Data,(void *)buf,Current->DataLength*sizeof(float));
+    //Current->Timer = NULL;
+    printf("Endpoint = %d\r\n",Current->Endpoint);
+}
 
 void StateMachine()
 {
@@ -22,24 +71,24 @@ void StateMachine()
         case Init:
             if(BufferSeek(frameflag,sizeof(frameflag) - 1))
                 break;//buffer not ready
-	    frameflag[sizeof(frameflag)] = '\0';
+	        frameflag[sizeof(frameflag)] = '\0';
             if(strcmp(FRAMEFLAG,frameflag))
             {
 	        printf("flag = %s\r\n",frameflag);
                 BufferRead(frameflag,1);//readpointer + 1
                 break;
             }
-	    //printf("Frame flag captured\r\n");
-            BufferRead(frameflag,sizeof(frameflag)-1);
+	        //printf("Frame flag captured\r\n");
+            BufferRead(frameflag,sizeof(frameflag) - 1);
             memset(frameflag,0,sizeof(frameflag));//frame flag found
             State = GetLen;
             break;
         case GetLen:
-	    //printf("GetLen state\r\n");
+	        //printf("GetLen state\r\n");
             if(BufferRead(&FrameLen,sizeof(FrameLen)))
                 break;//buffer not ready
             State = FramePick;
-	    //printf("FrameLen = %d\r\n",FrameLen);
+	        //printf("FrameLen = %d\r\n",FrameLen);
             fbuf = (uint8_t *)malloc(FrameLen - sizeof(FrameLen));
             break;//len got
         case FramePick:
@@ -52,7 +101,7 @@ void StateMachine()
                 sum += fbuf[i];
             }
             sum += ((uint8_t)FrameLen + (uint8_t)(FrameLen>>8));
-	    if(sum != fbuf[i])
+	        if(sum != fbuf[i])
             {
                 printf("checksum incorrect!\r\n");
                 FrameLen = 0;
@@ -66,16 +115,23 @@ void StateMachine()
                 printf("checksum pass!\r\n");
                 FrameLen = 0;
                 State = DataUpdate;
-		break;
+		        break;
             }
         case DataUpdate:
-	    printf("Procedure is waiting to be done\r\n");
-            free(fbuf);
+            if(*fbuf == REPORT)
+	    {
+		fbuf++;
+                DataPush(fbuf,FrameLen);
+		free(--fbuf);
+	    }
+	    else
+                free(fbuf);
             fbuf = NULL;
-	    State = Init;
+	        State = Init;
             break;
         default:
             printf("unexpected error!\r\n");
             break;
     }
 }
+
--- dataprocessing.h
+++ dataprocessing.h
@@ -1,5 +1,10 @@
 #ifndef DARAPROCESSING_H
 #define DARAPROCESSING_H
+
+//#include <linux/timer.h>
+
+//typedef struct timer_list timer; 
+
 enum states
 {
     Init = 0x00,
@@ -8,5 +13,33 @@ enum states
     DataUpdate = 0x03,
 };
 
+enum FrameType
+{
+    REPORT = 0x00,
+    CHECK = 0x01,
+    NETSTATUS = 0x02,
+};
+
+enum DataType
+{
+    _INT = 0x00,
+    _FLOAT = 0x01,
+};
+
+typedef struct ClusterStructure
+{
+    //timer * Timer;
+    uint8_t NodeID;
+    uint8_t ClusterID;
+    //char * Description;
+    uint8_t Endpoint;
+    uint8_t DataType;
+    uint8_t DataLength;
+    void * Data;
+    uint8_t ControlFlag;
+    struct ClusterStructure *next;
+}ClusterArray;
+
 void StateMachine();
+
 #endif
